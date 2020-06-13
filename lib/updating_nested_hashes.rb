aeea3a314148dddcd2307e614336cc7951b98d18
Hailey Ringier <haileyringier@outlook.com>
2020-06-13 17:20:28 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -47,4 +47,5 @@ def add_characters
    }
       epic_tragedy[:additional_characters][0] = { name: "Prince Escalus" }
       epic_tragedy[:additional_characters][1] = { name: "Apothecary" }
+    epic_tragedy  
 end
